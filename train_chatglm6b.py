8c74c7529694b08426092677cdafcee175894236
chenyw8 <likejava@qq.com>
2023-03-28 21:57:56 +0800

    random.sample error

--- train_chatglm6b.py
+++ train_chatglm6b.py
@@ -6,34 +6,15 @@ from MyTrainer import Trainer
 from transformers import TrainingArguments
 from transformers import DataCollatorForLanguageModeling
 import random
+import argparse
 from glob import glob
 from datasets import load_dataset
 from transformers import AutoTokenizer, AutoModel
 from peft import get_peft_model, LoraConfig, TaskType
 
-# 数据集目录
-DATASETS_PATH = "./data2/*"
-# 原始发布的预训练模型目录
-PRE_TRAINED_MODEL_PATH = "G:\idea_work2\chatglm-6b"
-tokenizer = AutoTokenizer.from_pretrained(PRE_TRAINED_MODEL_PATH, trust_remote_code=True)
-model = AutoModel.from_pretrained(PRE_TRAINED_MODEL_PATH, trust_remote_code=True).half().cuda()
-peft_config = LoraConfig(
-    task_type=TaskType.CAUSAL_LM, inference_mode=False, r=8, lora_alpha=32, lora_dropout=0.1,
-    target_modules=['query_key_value', ],
-)
-model = get_peft_model(model, peft_config)
-random.seed(42)
-all_file_list = glob(pathname=DATASETS_PATH)
-test_file_list = random.sample(all_file_list, 50)
-train_file_list = [i for i in all_file_list if i not in test_file_list]
-
-raw_datasets = load_dataset("csv", data_files={'train': train_file_list, 'valid': test_file_list},
-                            cache_dir="cache_data")
-
-context_length = 512
-
 
 def tokenize(element):
+    context_length = 512
     outputs = tokenizer(
         element["content"],
         truncation=True,
@@ -48,31 +29,61 @@ def tokenize(element):
     return {"input_ids": input_batch}
 
 
-tokenized_datasets = raw_datasets.map(tokenize, batched=True, remove_columns=raw_datasets["train"].column_names)
-data_collator = DataCollatorForLanguageModeling(tokenizer, mlm=False)
-args = TrainingArguments(
-    output_dir="test003",
-    per_device_train_batch_size=1,  # 如果在24G显存上的显卡，可以开到4
-    per_device_eval_batch_size=1,
-    evaluation_strategy="steps",
-    eval_steps=100,
-    logging_steps=100,
-    gradient_accumulation_steps=8,
-    num_train_epochs=1,
-    weight_decay=0.1,
-    warmup_steps=1_000,
-    lr_scheduler_type="cosine",
-    learning_rate=5e-4,
-    save_steps=100,
-    fp16=True,
-    push_to_hub=False,
-)
-trainer = Trainer(
-    model=model,
-    tokenizer=tokenizer,
-    args=args,
-    data_collator=data_collator,
-    train_dataset=tokenized_datasets["train"],
-    eval_dataset=tokenized_datasets["valid"],
-)
-trainer.train()
+def set_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset_path', default='./data2/*', type=str, required=False, help='数据集目录')
+    parser.add_argument('--model_path', default="../chatglm-6b", type=str, required=False,
+                        help='原始发布的预训练模型目录')
+    return parser.parse_args()
+
+
+def start_train(run_args):
+    tokenizer = AutoTokenizer.from_pretrained(run_args.model_path, trust_remote_code=True)
+    model = AutoModel.from_pretrained(run_args.model_path, trust_remote_code=True).half().cuda()
+    peft_config = LoraConfig(
+        task_type=TaskType.CAUSAL_LM, inference_mode=False, r=8, lora_alpha=32, lora_dropout=0.1,
+        target_modules=['query_key_value', ],
+    )
+    model = get_peft_model(model, peft_config)
+    random.seed(42)
+    all_file_list = glob(pathname=run_args.dataset_path)
+    test_file_list = random.sample(all_file_list, 50)
+    train_file_list = [i for i in all_file_list if i not in test_file_list]
+
+    raw_datasets = load_dataset("csv", data_files={'train': train_file_list, 'valid': test_file_list},
+                                cache_dir="cache_data")
+    tokenized_datasets = raw_datasets.map(tokenize, batched=True, remove_columns=raw_datasets["train"].column_names)
+    data_collator = DataCollatorForLanguageModeling(tokenizer, mlm=False)
+    args = TrainingArguments(
+        output_dir="test003",
+        per_device_train_batch_size=1,  # 如果在24G显存上的显卡，可以开到4
+        per_device_eval_batch_size=1,
+        evaluation_strategy="steps",
+        eval_steps=100,
+        logging_steps=100,
+        gradient_accumulation_steps=8,
+        num_train_epochs=1,
+        weight_decay=0.1,
+        warmup_steps=1_000,
+        lr_scheduler_type="cosine",
+        learning_rate=5e-4,
+        save_steps=100,
+        fp16=True,
+        push_to_hub=False,
+    )
+    trainer = Trainer(
+        model=model,
+        tokenizer=tokenizer,
+        args=args,
+        data_collator=data_collator,
+        train_dataset=tokenized_datasets["train"],
+        eval_dataset=tokenized_datasets["valid"],
+    )
+    print("start train...")
+    trainer.train()
+
+
+if __name__ == '__main__':
+    global run_args
+    run_args = set_args()
+    start_train(run_args)
