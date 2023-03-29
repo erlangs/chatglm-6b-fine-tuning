5ae11dd230f7e61fc337ae9f18ef246aef8ac6ae
chenyw8 <likejava@qq.com>
2023-03-29 22:49:19 +0800

    123

--- fine-tuning/train_chatglm6b.py
+++ fine-tuning/train_chatglm6b.py
@@ -1,3 +1,8 @@
+import os
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1,2,3,4"
+
 from MyTrainer import Trainer
 from transformers import TrainingArguments
 from transformers import DataCollatorForLanguageModeling
