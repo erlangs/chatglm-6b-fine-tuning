e22f0b5de918f5a9877db2175a4970a3477d5a3c
chenyw8 <likejava@qq.com>
2023-03-30 23:21:04 +0800

    123

--- finetuning/train_chatglm6b.py
+++ finetuning/train_chatglm6b.py
@@ -1,7 +1,3 @@
-import os
-
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "1,2,3,4"
 from MyTrainer import Trainer
 from transformers import TrainingArguments
 import random
