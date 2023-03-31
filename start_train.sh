d7bd85e6085ab0fcde503c03d5c02f280d753f82
chenyw8 <likejava@qq.com>
2023-03-31 23:29:15 +0800

    修改说明

--- start_train.sh
+++ start_train.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/thudm/chatglm-6b-fine-tuning
-nohup python3 ./fine-tuning/train_chatglm6b.py --model_path /home/thudm/chatglm-6b-fine-tuning/fine-tuning/pre-trained-model/ --dataset_path "/home/thudm/data/*" >>./logs/train.log 2>&1 &
+nohup python3 ./fine-tuning/fine_tuning_chatglm6b.py --model_path /home/thudm/chatglm-6b-fine-tuning/fine-tuning/pre-trained-model/ --dataset_path "/home/thudm/data/*" --batch_size 4 --fp16 >>./logs/train.log 2>&1 &
